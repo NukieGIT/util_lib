0d16b56d8c43d893d3c5d4ba6f27b06971bc348e
Nukie <92524294+NukieGIT@users.noreply.github.com>
2024-03-01 02:08:59 +0100

    Use `Path` instead of constructing it from strings

--- src/nuk/fileutil/FileWriter.java
+++ src/nuk/fileutil/FileWriter.java
@@ -4,21 +4,22 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.OpenOption;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Optional;
 
 public class FileWriter {
-    public static Optional<Path> writeToFile(String data, String pathToFile, String fileName, OpenOption... openOptions) {
-        Path path = Paths.get(pathToFile + "/" + fileName);
 
         if (!FileManager.doesDirectoryToFileExist(path)) return Optional.empty();
         if (!FileManager.doesFileExist(path)) return Optional.empty();
+    public static Optional<Path> writeToFile(String data, Path pathToFile, OpenOption... openOptions) {
+
+        if (!FileManager.doesDirectoryToFileExist(pathToFile)) return Optional.empty();
+        if (!FileManager.doesFileExist(pathToFile)) return Optional.empty();
 
         byte[] dataBytes = data.getBytes();
 
         try {
-            Files.write(path, dataBytes, openOptions);
-            return Optional.of(path);
+            Files.write(pathToFile, dataBytes, openOptions);
+            return Optional.of(pathToFile);
         } catch (IOException e) {
             System.out.println("Failed to write to file.");
             return Optional.empty();
@@ -28,20 +29,22 @@ public class FileWriter {
     public static Optional<Path> createAndWriteToFile(String data, String pathToFile, String fileName, OpenOption... openOptions) {
         Path path = Paths.get(pathToFile + "/" + fileName);
         Path dir = Paths.get(pathToFile);
+    public static Optional<Path> createAndWriteToFile(String data, Path pathToFile, OpenOption... openOptions) {
+        Path dir = pathToFile.getParent();
 
-        if (!FileManager.doesDirectoryToFileExist(path)) {
+        if (dir != null && !FileManager.doesDirectoryToFileExist(pathToFile)) {
             FileManager.createDirectoryRecursively(dir);
         }
 
-        if (!FileManager.doesFileExist(path)) {
-            FileManager.createFile(path);
+        if (!FileManager.doesFileExist(pathToFile)) {
+            FileManager.createFile(pathToFile);
         }
 
         byte[] dataBytes = data.getBytes();
 
         try {
-            Files.write(path, dataBytes, openOptions);
-            return Optional.of(path);
+            Files.write(pathToFile, dataBytes, openOptions);
+            return Optional.of(pathToFile);
         } catch (IOException e) {
             System.out.println("Failed to write to file.");
             return Optional.empty();
